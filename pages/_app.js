79b63a80fa3e3a5e873217459b7ed0c3bab1ef5c
Renny <connect.renny@gmail.com>
2023-05-08 18:55:45 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps, isHome }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      
       <Navbar />
       <Component {...pageProps} />
       <Footer />
