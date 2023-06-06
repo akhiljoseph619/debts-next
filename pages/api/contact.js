0932b1b2a55e7b46159d770433c8b86b1c3e43d5
akhiljoseph619 <akhiljoseph619@gmail.com>
2023-06-06 17:34:38 +0530

    iconchange

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -82,7 +82,7 @@ export default async function (req, res) {
             <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow: hidden; font-family: 'helvetica', 'ui-sans';">
                   </div>
                   <div class="container" style="margin-left: 20px;margin-right: 20px;">
-                  <h3>You've got a new mail from ${name}, their phone is: ✉️ ${email}, their phone is: ${phone} </h3>
+                  <h3>You've got a new mail from ${name}, their email is: ✉️ ${email}, their phone is: ☎️ ${phone} </h3>
                   <div style="font-size: 16px;">
                   <p>Message:</p>
                   <p>${message}</p>
